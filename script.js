da7f1d8ea0b23ebcb7100d24073bd3cb70085739
reddy-sravan12 <101791343+reddy-sravan12@users.noreply.github.com>
2025-05-07 14:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+  const out=[]
+  for(let i=0;i<arr.length-1;i++){
+	  let sum=arr[i]+arr[i+1]
+	  out.push(sum)
+  }
+return out.reduce((a,b)=>a+b)
 }
 
 module.exports=mincost;
